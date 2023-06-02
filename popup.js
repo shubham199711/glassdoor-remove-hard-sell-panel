2f2f73834de85cba2ae3883a7c1d4fc838d67f84
Shubham Dohare <sdohare11@gmail.com>
2023-06-02 11:08:05 +0530

    Minor fixes

--- popup.js
+++ popup.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
+    console.log("here ---------------->");
     var clearButton = document.getElementById('clearButton');
     clearButton.addEventListener('click', function () {
-        document.getElementById('HardsellOverlay').remove()
-        const body = document.getElementsByTagName("body")[0]
+        console.log("here 2 ---------------->");
+        var HardsellOverlay = document.getElementById('HardsellOverlay')
+        if (HardsellOverlay) {
+            HardsellOverlay.remove()
+        }
+        var body = document.getElementsByTagName("body")[0]
         body.style.overflow = 'auto'
         body.style.position = 'unset'
     });
